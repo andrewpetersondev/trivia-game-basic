657e49f0716b25e1bf9e6102a562acb19a646437
Andrew P <andrewpetersondev@gmail.com>
2019-06-01 20:27:32 -0500

    reduced whitespace and comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,74 +5,64 @@ $(document).ready(function () {
         {
             question: "What is the most populated city on Earth?",
             answers: ["Shanghai", "New York", "Istanbul", "Beijing"],
-            correctAnswer: "Shanghai"
+            correctAnswer: 0 // "Shanghai"
         },
         {
             question: "What country is Stonehenge located in?",
             answers: ["China", "Germany", "England", "France"],
-            correctAnswer: "England"
+            correctAnswer: 2 //  "England"
         },
         {
             question: "Which of the following cities is furthest east?",
             answers: ["Boston", "San Juan", "Miami", "New York"],
-            correctAnswer: "San Juan"
+            correctAnswer: 1 //  "San Juan"
         },
         {
             question: "What is the only city in the United States to have a royal palace?",
             answers: ["Salt Lake City", "Portland", "Boston", "Honolulu"],
-            correctAnswer: "Honolulu"
+            correctAnswer: 3 // "Honolulu"
         },
         {
             question: "What is statistically the safest way to travel?",
             answers: ["Car", "Train", "Plane", "Boat"],
-            correctAnswer: "Plane"
+            correctAnswer: 2 // "Plane"
         },
         {
             question: "Which country contains 60% of the world's lakes?",
             answers: ["Canada", "USA", "Russia", "China"],
-            correctAnswer: "Canada"
+            correctAnswer: 0 // "Canada"
         },
         {
             question: "An average person in which country consumes 22 pounds of chocalate every year?",
             answers: ["USA", "France", "Belgium", "Switzerland"],
-            correctAnswer: "Switzerland"
+            correctAnswer: 3 // "Switzerland"
         }
     ];
 
     // COUNTER VARIABLES
     var correctAnswerCount = 0;
     var incorrectAnswerCount = 0;
-    var unAnswerCount = questions.length; // = 0;
-    var questionIndex;
+    var unAnswerCount = 0;
 
     // TIMER VARIABLES
     var timer = 30;
     var intervalId;
     var isClockRunning = false;
 
-    // USER INPUT VARIABLES
-    var userAnswer;
-
-    $("#submitBtn").hide();
-    $("#results").hide();
-
-    function displayQuestions() {
+    function initializeGame() {
 
         // select the div with matching id 
         var gameDisplay = $("#game-display");
 
         // 1. create container to hold question and answer 
         for (var i = 0; i < questions.length; i++) {
-
             // create a div container for each question and answer
             var questionAnswerDiv = $("<div>");
             questionAnswerDiv.addClass("object-container");
             gameDisplay.append(questionAnswerDiv);
-
             var questionDiv = $("<div>" + questions[i].question + "</div>");
             questionDiv.addClass("question");
             questionAnswerDiv.append(questionDiv);
-
             // 2. create a container to hold answers and insert into questionAnswerDiv
             for (var j = 0; j < questions[i].answers.length; j++) {
                 var answerDiv = $("<div>" + questions[i].answers[j] + "</div>");
@@ -81,31 +71,55 @@ $(document).ready(function () {
                 var answerButton = $("<input>");
                 answerButton.addClass("answer-choice");
                 answerButton.attr("type", "radio");
-                answerButton.attr("value", j); // change this to work with strings
-                // answerButton.attr("button-value", questions[i].answers[j]);
+                answerButton.attr("value", j);
+                answerButton.attr("name", i);
                 answerDiv.append(answerButton);
                 questionAnswerDiv.append(answerDiv);
-
             }
-
         };
 
+        // hide the game display when the page loads
         $("#game-display").hide();
     }
 
-
-
-    // check if answer is correct
-    function checkAnswers() {
-        userAnswer = $(this).attr("value");
-        if (userAnswer === questions[i].correctAnswer) {
-            correctAnswerCount++;
-            console.log("correct answer count = " + correctAnswerCount);
+    // game over
+    function endGame() {
+        // loop through questions
+        for (var i = 0; i < questions.length; i++) {
+            // store user pick for each question
+            var userPick = $("input[name = " + i + "]:checked").val();
+            // console.log("user pick = " + userPick);
+            // console.log(userPick.val());
+            // if user pick does not exist
+            if (!userPick) {
+                unAnswerCount++;
+                continue;
+            }
+            // if user picked correct answer
+            if (questions[i].correctAnswer == userPick) {
+                // console.log('correct', i);
+                correctAnswerCount++;
+            }
+            // if user picked incorrect answer
+            else {
+                // console.log('NOT correct', i);
+                // console.log(questions[i]);
+                // console.log(questions[i].correctAnswer);
+                // console.log(userPick);
+                incorrectAnswerCount++;
+            }
         }
+        $("#game-display").hide();
+        displayResults();
+        // displayResults().show();
+        $("#results").show();
     }
 
-    // game over
-    // function gameOver () {}
+    function displayResults() {
+        $("#results").append("Correct Answers : " + correctAnswerCount);
+        $("#results").append("Incorrect Answers : " + incorrectAnswerCount);
+        $("#results").append("Unanswered : " + unAnswerCount);
+    }
 
     // function to start timer
     function startTimer() {
@@ -120,8 +134,7 @@ $(document).ready(function () {
         $("#timer").html("<h2> Time Remaining: " + timer + " Seconds </h2>");
         if (timer === 0) {
             stopTimer();
-            // lockGame();// ?
-            // gameOver();
+            endGame();
         }
     }
 
@@ -132,6 +145,9 @@ $(document).ready(function () {
 
     // ===================================== PLAY GAME ============================================
 
+    $("#submitBtn").hide();
+
+    $("#results").hide();
 
     // click start button
     $("#start").on("click", function () {
@@ -141,23 +157,11 @@ $(document).ready(function () {
         $("#submitBtn").show();
     });
 
-
-
-    displayQuestions();
-
-    // pick an answer
-    $(".answer-choice").on("click", function () {
-
-        // create a variable to hold the user answer
-        var userAnswer = $(this).val();
-        console.log("You clicked = " + userAnswer);
-
-        // checkAnswers();
-
-    })
+    initializeGame();
 
     $("#submitBtn").on("click", function () {
-        $("#results").show();
+        $("#submitBtn").hide();
+        endGame();
     })
 
 });
